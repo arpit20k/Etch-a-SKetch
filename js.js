76ce7ee034cd75db88eaac50d581d04becaf7a9c
Arpit Kumar <arpit20k@gmail.com>
2026-02-14 21:44:35 +0530

    basic working model made

--- js.js
+++ js.js
@@ -2,13 +2,36 @@
 let container = document.querySelector(".container");
 let controls = document.querySelector(".controls");
 let pixels = 64;
+let mousedown = false;
 
-for(let i=1;i<=pixels*pixels;i++)
+document.addEventListener("mousedown",()=>
+{
+    mousedown = true;
+});
+document.addEventListener("mouseup",()=>
+{
+    mousedown = false;
+});
+
+for(let i=0;i<pixels*pixels;i++)
 {
     const cell = document.createElement("div");
     cell.classList.add("cell");
     cell.style.width = (100/pixels)+"%";
     container.appendChild(cell);
+
+    cell.addEventListener("mouseenter",function()
+{
+    if(mousedown===true)
+    {
+        cell.style.backgroundColor = "black";
+    }
+});
+    cell.addEventListener("click",function()
+{
+        cell.style.backgroundColor = "black";
+});
 }
 
 let form = document.createElement("form");
+
